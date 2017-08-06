17afab4a99a08360048943961b394e322eb14c19
Michael Jewell <michaeljewell9911@gmail.com>
2017-08-05 18:40:05 -0700

    mj - move files around

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+import { Provider } from 'mobx-react';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import { Store } from './stores';
+
 ReactDOM.render(
-  <App />,
+  <Provider store={Store.create({})}>
+    <App />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
---
+++ src/stores/index.ts
@@ -0,0 +1,5 @@
+import { types } from 'mobx-state-tree';
+
+export const Store = types.model('Store', {
+  
+});
