2ecadad3b03bd799a530ffaa20e67bdf7a657739
Michael Jewell <michaeljewell9911@gmail.com>
2017-08-15 17:34:52 -0700

    mj - make graph and transactions observers

--- src/components/Transactions/index.tsx
+++ src/components/Transactions/index.tsx
@@ -1,8 +1,8 @@
-import { inject } from 'mobx-react';
+import { inject, observer } from 'mobx-react';
 import { compose } from 'recompose';
 
 import Presenter from './presenter';
 
-const container = compose(inject('store'));
+const container = compose(inject('store'), observer);
 
 export default container(Presenter);
