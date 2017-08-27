360403af8a5d3f362f19e827a83de46ec043cb34
Michael Jewell <michaeljewell9911@gmail.com>
2017-08-27 10:30:09 -0700

    mj - refactor transactions presenter

--- src/components/Transactions/presenter.tsx
+++ src/components/Transactions/presenter.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Button, Form } from 'react-bootstrap';
 
 import { IStore } from '../../stores/index';
+import { ITransaction } from '../../stores/transaction';
 import Transaction from '../Transaction';
 
 type IProps = {
@@ -14,17 +15,29 @@ export default class Transactions extends React.Component<IProps, {}> {
     removeTransaction(index);
   };
 
+  mapTransactionsToLines = (transactions: ITransaction[]) => {
+    return transactions.map((t, index) =>
+      <Form inline className="p-3" key={t.id as string}>
+        <Transaction transaction={t} />
+        <Button
+          bsStyle="danger"
+          className="ml-3"
+          onClick={this.removeTransaction(index)}
+        >
+          Delete
+        </Button>
+      </Form>
+    );
+  };
+
   render() {
     const { transactions, addTransaction } = this.props.store.transactions;
     return (
       <div>
-        {transactions.map((t, index) =>
-          <Form inline className="p-3" key={t.id as string}>
-            <Transaction transaction={t} />
-            <Button onClick={this.removeTransaction(index)}>Delete</Button>
-          </Form>
-        )}
-        <Button onClick={addTransaction}>Add Another Transaction</Button>
+        {this.mapTransactionsToLines(transactions)}
+        <Button bsStyle="success" onClick={addTransaction}>
+          Add Another Transaction
+        </Button>
       </div>
     );
   }
